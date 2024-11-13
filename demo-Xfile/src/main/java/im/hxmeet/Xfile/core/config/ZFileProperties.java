129c95b3808f9909adf99e6698673e01e8a89310
Amelia1018 <3349396150@qq.com>
2024-11-13 21:52:14 +0800

    11/13 second commit

--- demo-Xfile/src/main/java/im/hxmeet/Xfile/core/config/ZFileProperties.java
+++ demo-Xfile/src/main/java/im/hxmeet/Xfile/core/config/ZFileProperties.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 /**这个注解来自 Lombok 库，它自动为类生成常用的方法，如 getters、setters、toString、equals 和 hashCode 等。这简化了代码，使得我们不需要手动编写这些方法。*/
 @EnableConfigurationProperties
 @Component
+/**这个注解指定类将绑定到应用程序的配置文件（如 application.properties 或 application.yml）中以 zfile 为前缀的属性上。例如，如果有 zfile.debug=true，则该值将被自动映射到 debug 字段。*/
 @ConfigurationProperties(prefix = "zfile")
 public class ZFileProperties {
     private Boolean debug;
