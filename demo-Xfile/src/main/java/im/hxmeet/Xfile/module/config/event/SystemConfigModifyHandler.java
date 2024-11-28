dc1224b85e8a5cd540b64a0ef0df80761e22546e
Amelia1018 <3349396150@qq.com>
2024-11-28 16:14:10 +0800

    11/28 first commit

--- demo-Xfile/src/main/java/im/hxmeet/Xfile/module/config/event/SystemConfigModifyHandler.java
+++ demo-Xfile/src/main/java/im/hxmeet/Xfile/module/config/event/SystemConfigModifyHandler.java
@@ -1,4 +1,28 @@
 package im.hxmeet.Xfile.module.config.event;
 
-public class SystemConfigModifyHandler {
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+public class SystemConfigModifyHandler implements ISystemConfigModifyHandler {
+
+    @Resource
+    private List<ISystemConfigModifyHandler> handlers;
+
+    /*遍历注入的 handlers 列表，首先通过 filter 方法筛选出符合条件的处理器（即那些 matches 方法返回 true 的处理器），
+    然后通过 forEach 方法调用每个符合条件的处理器的 modify 方法，执行具体的修改操作。*/
+    public void modify(SystemConfig originalSystemConfig, SystemConfig newSystemConfig) {
+        handlers.stream()
+                .filter(handler -> handler.matches(originalSystemConfig.getName()))
+                .forEach(handler -> handler.modify(originalSystemConfig, newSystemConfig));
+    }
+
+    @Override
+    public boolean matches(String name) {
+        return true;
+    }
+
 }
+
